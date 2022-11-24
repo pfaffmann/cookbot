1a55d15875ea36fbd1f46044a96ba5455631f7da
pfaffic <48207288+pfaffmann@users.noreply.github.com>
2022-11-24 23:09:07 +0100

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,8 +6,8 @@ import { Telegraf } from 'telegraf';
 
 const bot = new Telegraf(process.env.COOKBOT_BOT_TOKEN as string);
 
-bot.start((ctx) => ctx.reply('Welcome'));
+bot.start((ctx) => ctx.reply('Welcome. send "hi" to test the bot'));
 bot.help((ctx) => ctx.reply('Send me a sticker'));
 bot.on('sticker', (ctx) => ctx.reply('❤️'));
-bot.hears('hi', (ctx) => ctx.reply(`Hallo ${ctx.from.first_name}`));
+bot.hears('hi', (ctx) => ctx.reply(`Hello ${ctx.from.first_name}`));
 bot.launch();
